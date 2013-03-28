1b9ddfb8426abd9eb651fcc68e99bafc055203b5
kpijewski <kpijewski@PLDL0037.rule.lan>
2013-03-28 09:56:46 +0100

    Block rotation calculated

--- tetris/src/krpi/boardgame/tetris/Block.java
+++ tetris/src/krpi/boardgame/tetris/Block.java
@@ -1,5 +1,6 @@
 package krpi.boardgame.tetris;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,6 +9,8 @@ public class Block {
     
     private final int x;
     private final int y;
+    private final float a;
+    private final float b;
     private final Orientation orientation;
     private final List<Tile> tiles;
     
@@ -27,10 +30,12 @@ public class Block {
         return new Block(x, y, orientation.rotate90CW());
     }
 
-    protected Block(int x, int y, Orientation orientations) {
+    protected Block(int x, int y, Orientation orientation) {
         this.x = x;
         this.y = y;
-        this.orientation = orientations;
+        this.a = x + 0.5f;
+        this.b = y + 1.5f;
+        this.orientation = orientation;
         this.tiles = Collections.unmodifiableList(createTiles());
     }
     
@@ -43,6 +48,18 @@ public class Block {
         }
     }
     
+    private List<Tile> createTilesEast() {
+        return rotateTiles(createTilesSouth(), a, b);
+    }
+
+    private List<Tile> createTilesSouth() {
+        return rotateTiles(createTilesWest(), a, b);
+    }
+
+    private List<Tile> createTilesWest() {
+        return rotateTiles(createTilesNorth(), a, b);
+    }
+    
     private List<Tile> createTilesNorth() {
         return Arrays.asList(
                 new Tile(x, y),
@@ -52,35 +69,17 @@ public class Block {
                 new Tile(x+1, y+3)
                 );
     }
-
-    private List<Tile> createTilesEast() {
-        return Arrays.asList(
-                new Tile(x, y+1),
-                new Tile(x+1, y+1),
-                new Tile(x+2, y+1),
-                new Tile(x+3, y+1),
-                new Tile(x+3, y)
-                );
+    
+    private static List<Tile> rotateTiles(List<Tile> tiles, float a, float b) {
+        List<Tile> result = new ArrayList<>(tiles.size());
+        for (Tile tile : tiles) {
+            result.add(rotateTile(tile,a , b));
+        }
+        return result;
     }
-
-    private List<Tile> createTilesSouth() {
-        return Arrays.asList(
-                new Tile(x, y),
-                new Tile(x+1, y),
-                new Tile(x+1, y+1),
-                new Tile(x+1, y+2),
-                new Tile(x+1, y+3)
-                );
-    }
-
-    private List<Tile> createTilesWest() {
-        return Arrays.asList(
-                new Tile(x, y+1),
-                new Tile(x, y),
-                new Tile(x+1, y),
-                new Tile(x+2, y),
-                new Tile(x+3, y)
-                );
+    
+    private static Tile rotateTile(Tile tile, float a, float b) {
+        return new Tile((int)(a-b)+tile.getY(), (int)(a+b)-tile.getX());
     }
 
     int getWidth() {
